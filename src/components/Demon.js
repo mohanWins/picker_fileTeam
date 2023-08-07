94d76bb105af923794cc804a26295961461a1221
MOHAN_SHARMA <mohansharmawins@gmail.com>
2023-08-07 09:57:52 +0530

    mohan create branch

--- src/components/Demon.js
+++ src/components/Demon.js
@@ -8,8 +8,11 @@ import { RxCross2 } from "react-icons/rx";
 import { AiOutlineCloudUpload } from "react-icons/ai";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import convertSize from "convert-size";
 
 function Demon() {
+
+
   //Modal start
 
   const [show, setShow] = useState(false);
@@ -39,7 +42,7 @@ function Demon() {
           })
         )
       );
-      //   console.log(acceptedFiles);
+ 
     },
   });
 
@@ -97,6 +100,7 @@ function Demon() {
   console.log(tyuObject);
 
   console.log("kjhresgfbxncmgvjhgfdbhtrdfg", u);
+ 
   return (
     <>
       <Modal show={show} onHide={handleClose} animation={true}>
@@ -116,14 +120,14 @@ function Demon() {
           <br></br>
           <div>
             <table className="table">
-              <thead>
+              {/* <thead>
                 <tr>
                   <th scope="col">icon </th>
                   <th scope="col">File</th>
                   <th scope="col">size</th>
                   <th scope="col">action</th>
                 </tr>
-              </thead>
+              </thead> */}
 
               {u.map((file, index) => {
                 return tyuObject[index] === "pdf" ? (
@@ -143,14 +147,16 @@ function Demon() {
                           />
                         </td>
                         <td>{file.path}</td>
-                        <td>{Number(file.size / 1024).toFixed(2)} kb</td>
+                        {/* <td>{Number(file.size / 1024).toFixed(2)} </td> */}
+                        <td>{convertSize(file.size, { accuracy: 2 })} </td>
                         <td onClick={() => deleteTask(index)}>
                           <RxCross2 style={{ color: "red" }} size={30} />
                         </td>
                       </tr>
                     </tbody>
                   </>
-                ) : (
+                ) : tyuObject[index] === "webp"? 
+                (
                   <>
                     <tbody>
                       <tr>
@@ -168,14 +174,73 @@ function Demon() {
                         </td>
                         <td>{file.path}</td>
                         {/* <td>{(file.size / 1024).toFixed(2)} kb</td> */}
-                        <td>{Number(file.size / 1024).toFixed(2)} kb</td>
+                        <td>{convertSize(file.size, { accuracy: 2 })}</td>
                         <td onClick={() => deleteTask(index)}>
                           <RxCross2 style={{ color: "red" }} size={30} />
                         </td>
                       </tr>
                     </tbody>
                   </>
-                );
+                )
+                :tyuObject[index] === "mpeg"?
+
+
+
+                (
+                  <>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <img
+                            src="https://i1.sndcdn.com/avatars-000312484264-af28qp-t500x500.jpg"
+                            alt="img"
+                            style={{
+                              width: "30px",
+                              height: "30px",
+                              borderRadius: "5px",
+                              marginLeft: "5px",
+                            }}
+                          />
+                        </td>
+                        <td>{file.path}</td>
+                        {/* <td>{(file.size / 1024).toFixed(2)} kb</td> */}
+                        <td>{convertSize(file.size, { accuracy: 2 })}</td>
+                        <td onClick={() => deleteTask(index)}>
+                          <RxCross2 style={{ color: "red" }} size={30} />
+                        </td>
+                      </tr>
+                    </tbody>
+                  </>
+                ):tyuObject[index] === "mp4"?
+                (
+                  <>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <img
+                            src="https://img.freepik.com/free-vector/flat-clapperboard-icon_1063-38.jpg"
+                            alt="img"
+                            style={{
+                              width: "30px",
+                              height: "30px",
+                              borderRadius: "5px",
+                              marginLeft: "5px",
+                            }}
+                          />
+                        </td>
+                        <td>{file.path}</td>
+                        {/* <td>{(file.size / 1024).toFixed(2)} kb</td> */}
+                        <td>{convertSize(file.size, { accuracy: 2 })}</td>
+                        <td onClick={() => deleteTask(index)}>
+                          <RxCross2 style={{ color: "red" }} size={30} />
+                        </td>
+                      </tr>
+                    </tbody>
+                  </>
+                ):console.log("jghfcvhj")
+
+
+
               })}
             </table>
           </div>
@@ -192,12 +257,13 @@ function Demon() {
 
       <Navbar className="bg-body-tertiary   fixed-top">
         <Container>
-          <Navbar.Brand href="#home">Drag n Drop</Navbar.Brand>
+          <Navbar.Brand  className="display-2   fs-1   text-success"    >Drag n Drop</Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
             <AiOutlinePlus
-              className="plus-icon"
+              className="plus-icon   text-primary "
               size={40}
+            
               onClick={handleShow}
             />
           </Navbar.Collapse>
@@ -208,43 +274,17 @@ function Demon() {
 
       {document
         ? u.map((file, index) => {
-            return tyuObject[index] === "pdf" ? (
+            return tyuObject[index] === "pdf" ?
+            
+            
+            (
               <div className="card mb-3  mx-5 shadow p-3 mb-5 bg-body rounded mx-auto card-sytle">
                 <div className="row g-3 ">
                   <div className="col-md-4">
-                    <img
-                      className="image-style"
-                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/1200px-PDF_file_icon.svg.png"
-                      alt=""
-                    ></img>
-                  </div>
-                  <div className="col-md-8 ">
-                    <div className="card-body">
-                      <h3 class="card-title text-secondary">File Details</h3>
-                      <hr></hr>
-                      <p className="card-text">
-                        <h5 className="text-danger">File</h5>
-                        {file.path}
-                      </p>
-                      <hr></hr>
-                      <p className="card-text">
-                        <h5 className="text-danger">File Size</h5>
-                        {Number(file.size / 1024).toFixed(2)} kb
-                      </p>
-                      <hr></hr>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ) : (
-              <div className="card mb-3  mx-5 shadow p-3 mb-5 bg-body rounded mx-auto card-sytle">
-                <div className="row g-3 ">
-                  <div className="col-md-4">
-                    <img
-                      className="image-style"
+                    <iframe className="image-style"
                       src={file.preview}
-                      alt=""
-                    ></img>
+                      title="mohan"
+                    ></iframe>
                   </div>
                   <div className="col-md-8 ">
                     <div className="card-body">
@@ -257,14 +297,134 @@ function Demon() {
                       <hr></hr>
                       <p className="card-text">
                         <h5 className="text-danger">File Size</h5>
-                        {Number(file.size / 1024).toFixed(2)} kb
+                        {convertSize(file.size, { accuracy: 2 })} 
                       </p>
                       <hr></hr>
                     </div>
                   </div>
                 </div>
               </div>
-            );
+            ) 
+            
+            : tyuObject[index] === "webp"? 
+            (
+              <div className="card mb-3  mx-5 shadow p-3 mb-5 bg-body rounded mx-auto card-sytle">
+                <div className="row g-3 ">
+                  <div className="col-md-4 "    style={{ display:'grid' ,placeItems:'center'    }}  >
+                    <img className="image-style"
+                      src={file.preview}
+                      alt="img"
+                    ></img>
+
+
+
+
+                    
+                  </div>
+                  <div className="col-md-8 ">
+                    <div className="card-body">
+                      <h3 class="card-title text-secondary">File Details</h3>
+                      <hr></hr>
+                      <p className="card-text">
+                        <h5 className="text-danger">File</h5>
+                        {file.path}
+                      </p>
+                      <hr></hr>
+                      <p className="card-text">
+                        <h5 className="text-danger">File Size</h5>
+                        {convertSize(file.size, { accuracy: 2 })} 
+                      </p>
+                      <hr></hr>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ) 
+
+
+            :tyuObject[index] === "mpeg"?
+
+
+
+            (
+          
+
+
+
+
+<div className="card mb-3  mx-5 shadow p-3 mb-5 bg-body rounded mx-auto card-sytle">
+                <div className="row g-3 ">
+                  <div className="col-md-4    rdx      "  
+      
+
+        >
+                    <audio className="image-style"
+                      src={file.preview}
+                 
+                      controls
+                    ></audio>
+                  </div>
+                  <div className="col-md-8 ">
+                    <div className="card-body">
+                      <h3 class="card-title text-secondary">File Details</h3>
+                      <hr></hr>
+                      <p className="card-text">
+                        <h5 className="text-danger">File</h5>
+                        {file.path}
+                      </p>
+                      <hr></hr>
+                      <p className="card-text">
+                        <h5 className="text-danger">File Size</h5>
+                        {convertSize(file.size, { accuracy: 2 })} 
+                      </p>
+                      <hr></hr>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+
+
+
+
+
+     
+            ) 
+            
+            
+            
+            
+            :tyuObject[index] === "mp4"?
+            (
+              <div className="card mb-3  mx-5 shadow p-3 mb-5 bg-body rounded mx-auto card-sytle">
+                <div className="row g-3 ">
+                  <div className="col-md-4">
+                    <video className="image-style"
+                      src={file.preview}
+                    controls
+                    ></video>
+                  </div>
+                  <div className="col-md-8 ">
+                    <div className="card-body">
+                      <h3 class="card-title text-secondary">File Details</h3>
+                      <hr></hr>
+                      <p className="card-text">
+                        <h5 className="text-danger">File</h5>
+                        {file.path}
+                      </p>
+                      <hr></hr>
+                      <p className="card-text">
+                        <h5 className="text-danger">File Size</h5>
+                        {convertSize(file.size, { accuracy: 2 })} 
+                      </p>
+                      <hr></hr>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ) 
+            
+            :console.log("jghfcvhj")
           })
         : console.log("kjkhgf")}
     </>
@@ -272,3 +432,4 @@ function Demon() {
 }
 
 export default Demon;
+
